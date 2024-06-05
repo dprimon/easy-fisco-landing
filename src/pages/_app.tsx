3344f7b21036255fcb614ab5525664406ddf7f7f
Daniele Primon <ledebass@gmail.com>
2024-06-05 16:13:11 +0200

    "mercoledì	05": auto take "202406051613"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
+import "@/styles/themes.scss";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    typeof document != undefined ? require('bootstrap/dist/js/bootstrap') : null
+  }, []);
+  return ( 
+    <> 
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>);
 }
